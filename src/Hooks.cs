68802784729346c101cb29ea322e6ec738a7fb3e
Contiinuum <68207985+Contiinuum@users.noreply.github.com>
2020-08-05 14:25:12 +0200

    updated for ML 0.2.3 and higher

---
+++ src/Hooks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using System.Reflection;
+using MelonLoader;
+
+namespace AudicaModding
+{
+    internal static class Hooks
+    {
+        public static void ApplyHooks(HarmonyInstance instance)
+        {
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+
+        [HarmonyPatch(typeof(KataConfig), "SetupGuns")]
+        private static class PatchKataSetupGuns
+        {
+            private static void Postfix()
+            {
+                if (PlayerCorral.I.rangeShell != 100)
+                    PlayerCorral.I.rangeShell = 100;
+            }
+        }
+    }
+}
