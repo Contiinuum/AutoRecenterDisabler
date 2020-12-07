9c99fbb985137d7d2e061c134797507be79ddc81
Contiinuum <68207985+Contiinuum@users.noreply.github.com>
2020-12-07 19:25:41 +0100

    version bump

--- src/Main.cs
+++ src/Main.cs
@@ -14,14 +14,13 @@ namespace AudicaModding
             public const string Name = "AutoRecenterDisabler";  // Name of the Mod.  (MUST BE SET)
             public const string Author = "Continuum"; // Author of the Mod.  (Set as null if none)
             public const string Company = null; // Company that made the Mod.  (Set as null if none)
-            public const string Version = "0.1.0"; // Version of the Mod.  (MUST BE SET)
+            public const string Version = "1.0.1"; // Version of the Mod.  (MUST BE SET)
             public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
         }
 
         public override void OnApplicationStart()
         {
             HarmonyInstance instance = HarmonyInstance.Create("AudicaMod");
-            Hooks.ApplyHooks(instance);
         }
     }
 }
